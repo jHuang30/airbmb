3cd904c05ca4d3450915c39bdffa63ee9e1b4857
App Academy Student <aa@Apps-Mac-mini.local>
2019-05-28 16:58:42 -0400

    userFeature

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.self.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password])
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render jason: ["Invalid info, pls try again."], status: 404
+        end
+    end
+
+    def destroy
+        if(current_user)
+            logout
+            render json: {}
+        else
+            render json: ["Not signed in"], status: 404
+        end
+    end
+
+end
